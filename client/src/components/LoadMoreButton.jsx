414be3a40e4f3c6e743e847db34b9ac7919e15b2
Lorena Ojeda <lorena.ojeda.u@gmail.com>
2025-04-10 11:43:58 +0200

    Load more button

---
+++ client/src/components/LoadMoreButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/LoadMoreButton.scss"; 
+
+const LoadMoreButton = ({ onClick }) => {
+  return (
+    <div className="load-more-container">
+      <button className="load-more-button" onClick={onClick}>
+        More
+      </button>
+    </div>
+  );
+};
+
+export default LoadMoreButton;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,8 +3,13 @@ import SearchBar from "../components/SearchBar";
 import Sidebar from "../components/SideFilters"; 
 import "../styles/HomePage.scss";
 import InfoSection from "../components/InfoSection";
+import LoadMoreButton from "../components/LoadMoreButton";
 
 function HomePage() {
+    const handleLoadMore = () => {
+        console.log("Load more clicked!");
+      };
+    
     return (
         <>
             <div className="home-page-container">
@@ -70,15 +75,7 @@ function HomePage() {
                         >
                         </CourseCard>
 
-                        <CourseCard
-                            title="Frontend Developer"
-                            description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
-                            location="Malmö"
-                            startDate="Sep 2025"
-                            csnEligible={true}
-                            status="closed"
-                        >
-                        </CourseCard>
+                        <LoadMoreButton onClick={handleLoadMore} />
                     </div>
                     <div>
                         <InfoSection />
