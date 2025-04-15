6b98e6745b1352bc9ac72abe416fa6b91c0aee88
Lorena Ojeda <lorena.ojeda.u@gmail.com>
2025-04-15 10:40:32 +0200

    old code restored

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -75,3 +75,109 @@ function HomePage() {
 }
 
 export default HomePage;
+
+/* import React, { useState } from "react";
+import CourseCard from "../components/CourseCard";
+import SearchBar from "../components/SearchBar";
+import Sidebar from "../components/SideFilters";
+import InfoSection from "../components/InfoSection";
+import LoadMoreButton from "../components/LoadMoreButton";
+import "../styles/HomePage.scss";
+
+function HomePage() {
+  const courses = [
+    {
+      title: "Frontend Developer",
+      description: "Lorem ipsum dolor sit amet...",
+      location: "Malmö",
+      startDate: "Sep 2025",
+      csnEligible: true,
+      status: "closed",
+    },
+    {
+      title: "Frontend Developer",
+      description: "Lorem ipsum dolor sit amet...",
+      location: "Malmö",
+      startDate: "Sep 2025",
+      csnEligible: true,
+      status: "open",
+    },
+    {
+      title: "Frontend Developer",
+      description: "Lorem ipsum dolor sit amet...",
+      location: "Malmö",
+      startDate: "Sep 2025",
+      csnEligible: true,
+      status: "late",
+    },
+    {
+      title: "Frontend Developer",
+      description: "Lorem ipsum dolor sit amet...",
+      location: "Malmö",
+      startDate: "Sep 2025",
+      csnEligible: true,
+      status: "open",
+    },
+    {
+      title: "Frontend Developer",
+      description: "Lorem ipsum dolor sit amet...",
+      location: "Malmö",
+      startDate: "Sep 2025",
+      csnEligible: true,
+      status: "closed",
+    },
+  ];
+
+  const [visibleCourses, setVisibleCourses] = useState(3); 
+
+  const handleLoadMore = () => {
+    setVisibleCourses((prev) => prev + 2); 
+  };
+
+  return (
+    <div className="home-page-container">
+      <nav>
+        <p>navbar</p>
+      </nav>
+
+      <aside>
+        <Sidebar />
+      </aside>
+
+      <main>
+        <div>
+          <SearchBar />
+        </div>
+
+        <div className="results-container">
+          {courses.slice(0, visibleCourses).map((course, index) => (
+            <CourseCard
+              key={index}
+              title={course.title}
+              description={course.description}
+              location={course.location}
+              startDate={course.startDate}
+              csnEligible={course.csnEligible}
+              status={course.status}
+            />
+          ))}
+
+          {visibleCourses < courses.length && (
+            <LoadMoreButton onClick={handleLoadMore} />
+          )}
+        </div>
+
+        <div>
+          <InfoSection />
+        </div>
+      </main>
+
+      <footer>
+        <p>footer</p>
+      </footer>
+    </div>
+  );
+}
+
+export default HomePage;
+*/ 
