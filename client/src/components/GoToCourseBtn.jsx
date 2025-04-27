afa0fc8fd858c78337333ca480d2cbdc0f5203f2
Lorena Ojeda <lorena.ojeda.u@gmail.com>
2025-04-27 13:07:27 +0200

    course page linked from course card btn

--- client/src/components/GoToCourseBtn.jsx
+++ client/src/components/GoToCourseBtn.jsx
@@ -1,4 +1,4 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+/*import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import '../styles/GoToCourseBtn.scss';
 function GoToCourseBtn() {
@@ -9,6 +9,23 @@ function GoToCourseBtn() {
                 <button className="btnGo"><FontAwesomeIcon icon={faChevronRight} /></button>
             </div>
         </>
+    );
+}
+
+export default GoToCourseBtn;*/
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom'; // IMPORTANT: import Link
+import '../styles/GoToCourseBtn.scss';
+
+function GoToCourseBtn({ courseId }) { // receive the courseId!
+    return (
+        <Link to={`/course/${courseId}`} style={{ textDecoration: "none" }}>
+            <button className="btnGo">
+                <FontAwesomeIcon icon={faChevronRight} />
+            </button>
+        </Link>
     );
 }
 
